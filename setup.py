c77cce644a6a6b4ed785e824a17279ab647deccb
Stuart Pearce <spearce@coas.oregonstate.edu>
2019-09-06 11:11:52 -0700

    updated trying to get install to work

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
 from distutils.core import setup
+from setuptools import find_packages
+
+packages=find_packages()
 
 setup(
-    name='glider_utils',
-    version='1.0',
+    name='glider-utilities',
+    version='1.2',
     description='Suite of Slocum Glider Python Utilities',
+    license='LICENSE',
     author='Stuart Pearce',
-    author_email='spearce@coas.oregonstate.edu',
-    py_modules=[
-        'argos', 'conv', 'geo', 'seawater2', 'seawater3',
-        'parsers.dbd_parsers', 'parsers.dvl_parsers', 
-        'parsers.gliderstate_parser', 'parsers.log_parser',
-        'plots.enduranceMaps', 'plots.plots', 'plots.SingleGliderTracks',
-        'stats.days_of_gldata.'],
-    package_data={},
-    requires=['numpy', 'matplotlib', 'pandas', 'gsw'],
+    author_email='spearce@ceoas.oregonstate.edu',
+    keywords=['oceanography', 'Slocum gliders', 'gliders','seawater'],
+    packages=packages,
+#    py_modules=[
+#        'glider_utils.argos', 'conv', 'geo', 'seawater2', 'seawater3',
+#        'parsers.dbd_parsers', 'parsers.dvl_parsers', 
+#        'parsers.gliderstate_parser', 'parsers.log_parser',
+#        'plots.enduranceMaps', 'plots.plots', 'plots.SingleGliderTracks',
+#        'stats.days_of_gldata.'],
+    package_data={'glider_utils.plots.ETOPO1.mat},
+    requires=['numpy', 'matplotlib', 'pandas', 'gsw',
+    'mpl_toolkits.basemap'],
 )
