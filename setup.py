c7cb5f11130c8e1b6e056020908480c84ff715f1
Stuart Pearce <spearce@coas.oregonstate.edu>
2019-09-06 10:50:07 -0700

    trying to get the installer to work

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from distutils.core import setup
 
 setup(
-    name='compass_check',
+    name='glider_utils',
     version='1.0',
-    description='Glider Compass Checking Program',
+    description='Suite of Slocum Glider Python Utilities',
     author='Stuart Pearce',
     author_email='spearce@coas.oregonstate.edu',
     py_modules=[
-        'compass_check', 'cc.serial_rf',
-        'cc.parse_options', 'cc.dockserver_com'],
-    package_data={'cc': ['pickles/']},
-    requires=['numpy', 'matplotlib', 'serial', 'dockserverTalk'],
+        'argos', 'conv', 'geo', 'seawater2', 'seawater3',
+        'parsers.dbd_parsers', 'parsers.dvl_parsers', 
+        'parsers.gliderstate_parser', 'parsers.log_parser',
+        'plots.enduranceMaps', 'plots.plots', 'plots.SingleGliderTracks',
+        'stats.days_of_gldata.'],
+    package_data={},
+    requires=['numpy', 'matplotlib', 'pandas', 'gsw'],
 )
