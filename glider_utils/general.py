fce15b5e0b670473e22dd419bb777982888cb78b
Stuart Pearce <stuart.pearce@oregonstate.edu>
2023-09-27 21:48:47 -0700

    adds a sorting function for lists of glider data files

--- glider_utils/general.py
+++ glider_utils/general.py
@@ -1,8 +1,11 @@
-import pandas as pd
+import re
+import logging
 from datetime import datetime as dt
 from dateutils.parser import parser as dtparse
 import numpy as np
+import pandas as pd
 from glider_utils.parsers.dbd_parsers import get_fileopen_time
+
 
 def mts(timestr):
     """returns a unix timestamp in seconds since Jan 1, 1970"""
@@ -31,7 +34,7 @@ def gliderfile_from_time(ts, files):
         ts = dtparse(ts)
     elif isinstance(ts, (int, float)):
         ts = dt.utcfromtimestamp(ts)
-    elif isinstance(ts, dt):
+    #elif isinstance(ts, dt):
     opentimes = np.empty(len(files), dtype='datetime64[ns]')
     for ii, file in enumerate(files):
         opentime = get_fileopen_time(file)
@@ -44,3 +47,25 @@ def gliderfile_from_time(ts, files):
     
     file_index = np.flatnonzero(sorted_ots < timex)[-1]
     return sorted_files[file_index]
+
+slocumregex = (
+    r'[a-z_0-9]+(?:_|-)(\d{4}(?:_|-)\d{3}(?:_|-))(\d{1,2})(?:_|-)'
+    r'(\d{1,4})\..+')
+regex = re.compile(slocumregex)
+
+
+def sort_function(filename):
+    fname = os.path.basename(filename)
+    match = regex.search(fname)
+    if not match:
+        error_msg = (
+            'File {:s} does not match the Slocum filename format.'.format(
+                filename
+            ))
+        logging.warning(error_msg)
+        raise ValueError(error_msg)
+    else:
+        mission_num = '{:02d}'.format(int(match.group(2)))
+        segment_num = '{:04d}'.format(int(match.group(3)))
+        sort_str = match.group(1) + mission_num + '_' + segment_num
+    return sort_str
